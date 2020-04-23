4d2b4ac578f77d6a480f4307c115fa871a711324
naufal025 <53498520+naufal025@users.noreply.github.com>
2020-04-23 13:51:32 +0700

    Add files via upload

---
+++ on-member/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Checkup Yourself</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+<div class = "row"> 
+<div class = "col-md-6">
+          <h3>Saya Naufal Gajendra Ardiputra, dan saya bersekolah 
+                di SMK Telkom Malang dari kelas X RPL 2, Saya berasal dari  Kota malang</h3>
+        </div>
+<div class = "col-md-6">
+            <img src="IMG-20190615-WA0060.jpg" width="700" height="450" >
+</div>
+</div>
+       
+       
+  <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ on-member/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Checkup Yourself</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+    <h2 align="center"><font color = "blue">Self Checkup</font></h2>
+    <br>
+    <h3>Salam Sehat!</h3>
+    <div class="row">
+        <div class="col-md-6">
+        <p>Untuk mencegah penyebaran covid-19, maka diperlukan kesadaran dari masyarakat maupun diri sendiri
+        dengan mengikuti semua anjuran pemerintah,maka dari itu mari kita cek diri kita sendiri
+        </p>  
+   <a href ="check.php" class ="btn btn-info">Cek Sekarang</a>
+   <a href="about.php" class="btn btn-success">About Creator</a>
+        </div>
+        <div class="col-md-6">
+            <img src="1.jpg" width="600" height="400">
+           
+        </div>
+    </div>
+    </div>   
+  <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
